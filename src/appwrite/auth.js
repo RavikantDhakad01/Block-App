f6664d687a96b549c404bbfc843990a44d836f8c
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-01-15 19:07:30 +0530

    create auth service

---
+++ src/appwrite/auth.js
@@ -0,0 +1,56 @@
+import conf from "../confg/confg";
+import { Client, Account, ID } from "appwrite";
+
+class AuthService {
+
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteurl)
+            .setProject(conf.appwriteProjectId)
+        this.account = new Account(this.client)
+    }
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if (userAccount) {
+                return this.login({ email, password })
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            return await this.account.createSession(email, password)
+        } catch (error) {
+            throw error
+        }
+    }
+
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get()
+        } catch (error) {
+            console.log(error.message)
+        }
+        return null
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+}
+
+const authService = new AuthService()
+export default authService
