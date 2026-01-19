7f5d5ff9f09603d877fb3696c95a80a40d6d91dc
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-01-19 08:51:27 +0530

    Add Edit post page x2

---
+++ src/Components/pages/EditPost.jsx
@@ -0,0 +1,32 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import service from '../../appwrite/config'
+import { useEffect, useState } from 'react';
+import { PostForm, Container } from '../index'
+
+function EditPost() {
+
+    const navigate = useNavigate()
+    const { slug } = useParams
+    const [post, setPost] = useState()
+
+    useEffect(() => {
+        if (slug) {
+            service.getPost(slug).then((post) => {
+                if (post) {
+                    setPost(post)
+                }
+            })
+        }
+        else {
+            navigate('/')
+        }
+    }, [slug])
+    return post ? (
+        <div className='py-8'>
+            <Container>
+                <PostForm post={post} />
+            </Container>
+        </div>
+    ) : null
+}
+export default EditPost
