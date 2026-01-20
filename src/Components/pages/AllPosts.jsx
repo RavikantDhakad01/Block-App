bb2503ed37fcae3d1a45ac3cbb179348f28c10c4
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-01-20 21:27:26 +0530

    add seprate user posts

--- src/Components/pages/AllPosts.jsx
+++ src/Components/pages/AllPosts.jsx
@@ -1,17 +1,24 @@
 import { useEffect, useState } from "react";
 import { Container, PostCard } from '../index'
 import service from "../../appwrite/config";
+import { useSelector } from "react-redux";
+
 function AllPosts() {
+    const userData =useSelector((state)=>state.auth.user)
     const [Posts, setPosts] = useState([])
+
     useEffect(() => {
-        service.getPost([])
+        if(!userData){
+            return
+        }
+        service.getPosts(userData.$id)
             .then((posts) => {
                 if (posts) {
                     setPosts(posts.documents)
                 }
 
             })
-    }, [])
+    }, [userData])
     return (
         <div className='w-full py-8'>
             <Container>
