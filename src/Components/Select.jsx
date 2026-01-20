600d9d7f4e704695ddffed37bdafcd39a3bd98a3
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-01-20 21:43:05 +0530

    Add cursor pointer

--- src/Components/Select.jsx
+++ src/Components/Select.jsx
@@ -8,7 +8,7 @@ function Select({
             {label && <label htmlFor={id}>
                 {label}
             </label>}
-            <select className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`} id={id} ref={ref} {...props}>
+            <select className={`cursor-pointer px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`} id={id} ref={ref} {...props}>
                 {
                     options?.map((option) => {
                         return <option key={option} value={option}>{option}</option>
